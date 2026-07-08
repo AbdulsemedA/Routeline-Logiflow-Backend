8a0f5c8601ffe2e2be5f508ad6229dfd0394f93e
AbdulsemedA <abdulsemedabdelila65@gmail.com>
2026-07-08 14:42:06 +0300

    feat: implement user registration with email verification and configure base Django settings

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,15 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = env("EMAIL_BACKEND", default="django.core.mail.backends.console.EmailBackend")
+EMAIL_HOST = env("EMAIL_HOST", default="")
+EMAIL_PORT = env.int("EMAIL_PORT", default=587)
+EMAIL_USE_TLS = env.bool("EMAIL_USE_TLS", default=True)
+EMAIL_HOST_USER = env("EMAIL_HOST_USER", default="")
+EMAIL_HOST_PASSWORD = env("EMAIL_HOST_PASSWORD", default="")
+DEFAULT_FROM_EMAIL = env("DEFAULT_FROM_EMAIL", default="noreply@routeline.io")
+
+FRONTEND_URL = env("FRONTEND_URL", default="http://localhost:8080")
 
 STORAGES = {
     "staticfiles": {
--- logiflow/accounts/serializers.py
+++ logiflow/accounts/serializers.py
@@ -3,6 +3,7 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 from django.core.mail import send_mail
+from django.conf import settings
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
@@ -22,11 +23,11 @@ class RegisterSerializer(serializers.ModelSerializer):
         token = default_token_generator.make_token(user)
         uid = urlsafe_base64_encode(force_bytes(user.pk))
         
-        verification_link = f"http://localhost:5173/verify-email?uidb64={uid}&token={token}"
+        verification_link = f"{settings.FRONTEND_URL}/verify-email?uidb64={uid}&token={token}"
         send_mail(
             "Verify your Routeline account",
             f"Please click the link below to verify your email address:\n\n{verification_link}",
-            "noreply@routeline.io",
+            settings.DEFAULT_FROM_EMAIL,
             [user.email],
             fail_silently=False,
         )
